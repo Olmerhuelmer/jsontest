200ed7d13d0f8e644d5ad82c810aa451c7e15146
Olmerhuelmer <48070875+Olmerhuelmer@users.noreply.github.com>
2020-06-16 14:27:57 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,15 +3,24 @@ import requests
 import sys
 
 def get_api():
+	with open(sys.argv[1]) as f:
+		ready_json = json.load(f)
 	n = 0
-	while True:
+	while n < 5:
 		n += 1
 		response = requests.get("https://jsonplaceholder.typicode.com/posts/" + str(n))
 		data_json = response.json()
 		def retr_modif_dict(data_json):
-			#data_json.setdefault("body", 'body-' + str(data_json['id']))
-			data_json['body'] = 'body-' + str(data_json['id'])
-		retr_modif_dict(data_json)
-		print (data_json)
+			return data_json['body']
+		
+		def deep_layer(test_dict):
+			internal_dict = test_dict['replies']
+			test_dict.setdefault('body', 'body-' + retr_modif_dict(data_json))
+			if len(internal_dict) != 0:
+				for i in internal_dict:
+					deep_layer(i)
+		dict_data = deep_layer(ready_json)
+	print (ready_json)
+
 if __name__ == '__main__':
 	get_api()
