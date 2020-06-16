6a0d1a19ec72cca677a2278e6ab53c9796c83cb3
Olmerhuelmer <48070875+Olmerhuelmer@users.noreply.github.com>
2020-06-16 16:54:39 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -5,27 +5,20 @@ import sys
 def get_api(a):
 	#with open(sys.argv[1]) as f:
 	ready_json = a
-	def retr_modif_dict():
-		n = 0
-		list_comm = []
-		while n < 5:
-			n += 1
-			response = requests.get("https://jsonplaceholder.typicode.com/posts/" + str(n))
-			data_json = response.json()
-			list_comm.append(data_json['body'])
-		return list_comm
+	def retr_modif_dict(n):
+		response = requests.get("https://jsonplaceholder.typicode.com/posts/" + str(n))
+		data_json = response.json()
+		body_comm = data_json['body']
+		return body_comm
 			
-			 
-	lst_comm = retr_modif_dict()
-	def deep_layer(test_dict,n):
+	def deep_layer(test_dict):
 		internal_dict = test_dict['replies']
-		test_dict['body'] = 'body-' + str(lst_comm[n])	
+		id_key = test_dict['id']
+		test_dict['body'] = 'body-' + retr_modif_dict(id_key)	
 		if len(internal_dict) != 0:
 			for i in internal_dict:
-				n += 1
-				deep_layer(i,n)
-	n = 0
-	dict_data = deep_layer(ready_json,n)
+				deep_layer(i)
+	dict_data = deep_layer(ready_json)
 	print (ready_json)
 
 if __name__ == '__main__':
