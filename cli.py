db6d08e4d2a8e46d197b1c5ad22050f948719318
Olmerhuelmer <48070875+Olmerhuelmer@users.noreply.github.com>
2020-06-16 16:40:57 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,9 +2,9 @@ import json
 import requests
 import sys
 
-def get_api():
-	with open(sys.argv[1]) as f:
-		ready_json = json.load(f)
+def get_api(a):
+	#with open(sys.argv[1]) as f:
+	ready_json = a
 	def retr_modif_dict():
 		n = 0
 		list_comm = []
@@ -17,15 +17,27 @@ def get_api():
 			
 			 
 	lst_comm = retr_modif_dict()
-
-	def deep_layer(test_dict):
+	def deep_layer(test_dict,n):
 		internal_dict = test_dict['replies']
-		test_dict['body'] = 'body-' + str(lst_comm[test_dict['id'] - 1])	
+		test_dict['body'] = 'body-' + str(lst_comm[n])	
 		if len(internal_dict) != 0:
 			for i in internal_dict:
-				deep_layer(i)
-	dict_data = deep_layer(ready_json)
+				n += 1
+				deep_layer(i,n)
+	n = 0
+	dict_data = deep_layer(ready_json,n)
 	print (ready_json)
 
 if __name__ == '__main__':
-	get_api()
+	def red_full_json():
+		b = str()
+		try:
+			while True:
+				a = input()
+				b += a
+				#print (b,' g')
+		except EOFError:
+			c = b
+		return eval(c)
+	a = red_full_json()
+	get_api(a)
