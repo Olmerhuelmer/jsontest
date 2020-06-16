a780eaa326ef939f24316ed3ae5502dcdba57679
Olmerhuelmer <48070875+Olmerhuelmer@users.noreply.github.com>
2020-06-16 14:12:38 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,23 +3,14 @@ import requests
 import sys
 
 def get_api():
-	with open(sys.argv[1]) as f:
-		data_json = json.load(f)
-	#response = sys.argv[1]
-	#print (response)
-	#ready_json = {"id": 1, "replies": [{"id": 2, "replies": []}, {"id": 3,"replies": [{"id": 4,"replies": []}, {"id": 5,"replies": []}]}]}
-	#response = requests.get("https://jsonplaceholder.typicode.com/posts/ID ")
-	#data_json = ready_json
-	def deep_layer(test_dict):
-		internal_dict = test_dict['replies']
-		test_dict.setdefault('body', 'body-' + str(test_dict['id']))
-		if len(internal_dict) != 0:
-			for i in internal_dict:
-				deep_layer(i)
-	dict_data = deep_layer(data_json)
-	print (data_json)
-
-
+	while True:
+		n += 1
+		response = requests.get("https://jsonplaceholder.typicode.com/posts/" + str(n))
+		data_json = response.json()
+		def retr_modif_dict(data_json):
+			#data_json.setdefault("body", 'body-' + str(data_json['id']))
+			data_json['body'] = 'body-' + str(data_json['id'])
+		retr_modif_dict(data_json)
+		print (data_json)
 if __name__ == '__main__':
 	get_api()
-
