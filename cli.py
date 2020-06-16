2cf82387cb2874032584fd45bc01299eb6563faa
Olmerhuelmer <48070875+Olmerhuelmer@users.noreply.github.com>
2020-06-16 21:43:32 +0300

    Update cli.py

--- cli.py
+++ cli.py
@@ -6,7 +6,6 @@ from multiprocessing.dummy import Pool as ThreadPool
 def get_api(a):
 	def get_comment(ls_id):
 		def retr_modif_dict(addr):
-			#response = requests.get("https://jsonplaceholder.typicode.com/posts/" + str(n))
 			response = requests.get(addr)
 			data_json = response.json()
 			body_comm = data_json['body']
@@ -18,7 +17,7 @@ def get_api(a):
 		pool.close()
 		pool.join()
 		return results
-
+	############################
 	ready_json = a
 	list_id = []
 	#############################
@@ -30,22 +29,21 @@ def get_api(a):
 			for i in internal_dict:
 				numb_id(i)
 		return list_id
+	#############################
 	numb_id(ready_json)
 	list_comments = get_comment(list_id)
-	n = 0
 	##############################
-	def deep_layer(test_dict,n):
+	def deep_layer(test_dict):
 		internal_dict = test_dict['replies']
 		id_key = test_dict['id']
-		#test_dict['body'] = 'body-' + retr_modif_dict(id_key)	
-		test_dict['body'] = 'body-' + str(list_comments[n])
+		index_comm = list_id.index(id_key)
+		test_dict['body'] = 'body-' + str(list_comments[index_comm])
 		if len(internal_dict) != 0:
 			for i in internal_dict:
-				n += 1
-				deep_layer(i,n)
+				deep_layer(i)
 	#############################
 	
-	deep_layer(ready_json,n)
+	deep_layer(ready_json)
 	print (json.dumps(ready_json))
 
 if __name__ == '__main__':
